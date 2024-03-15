c035dd4d871e9c375854915b1f22f2e370700573
Cardoso88 <alexmcardoso@gmail.com>
2024-03-14 22:53:48 -0300

    feat: add schemas

---
+++ schemas/schemas.go
@@ -0,0 +1,24 @@
+package schemas
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Owner struct {
+	gorm.Model
+	Name  string
+	Email string
+	Pets  []Pet
+}
+
+type Pet struct {
+	gorm.Model
+	Name      string
+	Breed     string
+	Birthday  time.Time
+	Sex       string
+	Available bool
+	OwnerID   uint
+}

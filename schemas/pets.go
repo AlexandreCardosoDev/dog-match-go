ba24333c75434426d14ad9c3a58ea432ba770617
Cardoso88 <alexmcardoso@gmail.com>
2024-03-16 20:07:52 -0300

    feat: add response struct

--- schemas/pets.go
+++ schemas/pets.go
@@ -22,3 +22,25 @@ type Owner struct {
 	Tel   string
 	Pets  []Pet
 }
+
+type PetResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Name      string    `json:"name"`
+	Breed     string    `json:"breed"`
+	Birthday  time.Time `json:"birthday"`
+	Sex       string    `json:"sex"`
+	OwnerID   uint      `json:"ownerId"`
+}
+
+type OwnerResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Tel       string    `json:"tel"`
+}

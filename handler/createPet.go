12651155b8e81d377a6a3082377d742155d6c8f6
Cardoso88 <alexmcardoso@gmail.com>
2024-03-20 17:05:10 -0300

    feat: add createPet logic

--- handler/createPet.go
+++ handler/createPet.go
@@ -2,12 +2,37 @@ package handler
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/AlexandreCardosoDev/dog-match-go/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func CreatePetHanler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Create pet",
-	})
+func CreatePetHanler(ctx *gin.Context) {
+	request := CreatePetRequest{}
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Validation error: %v", err)
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	birthdayRequest, _ := time.Parse("2006-01-02", request.Birthday)
+
+	pet := schemas.Pet{
+		Name:     request.Name,
+		Breed:    request.Breed,
+		Birthday: birthdayRequest,
+		Sex:      request.Sex,
+		OwnerID:  request.OwnerID,
+	}
+
+	if err := db.Create(&pet).Error; err != nil {
+		logger.Errorf("error creating pet %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating pet on database")
+		return
+	}
+
+	sendSuccess(ctx, "create-pet", pet)
 }

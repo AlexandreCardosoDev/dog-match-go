1ad11dd9ca5e578a17b6a27724a730b3235f293a
Cardoso88 <alexmcardoso@gmail.com>
2024-03-20 19:21:23 -0300

    feat: add listPet logic

--- handler/listPets.go
+++ handler/listPets.go
@@ -3,11 +3,18 @@ package handler
 import (
 	"net/http"
 
+	"github.com/AlexandreCardosoDev/dog-match-go/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func ListPetHanler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "list pet",
-	})
+func ListPetHanler(ctx *gin.Context) {
+	pet := []schemas.Pet{}
+
+	//Get all pets
+	if err := db.Find(&pet).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing pets")
+		return
+	}
+
+	sendSuccess(ctx, "list-pet", pet)
 }

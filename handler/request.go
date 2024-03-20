88b85d38cdacf583fcf46796965c35ba12150add
Cardoso88 <alexmcardoso@gmail.com>
2024-03-20 17:04:43 -0300

    fix: pet request

--- handler/request.go
+++ handler/request.go
@@ -12,15 +12,15 @@ func errParamIsRequired(name, typ string) error {
 //CreatePets
 
 type CreatePetRequest struct {
-	Name     string    `json:"name"`
-	Breed    string    `json:"breed"`
-	Birthday time.Time `json:"brithday"`
-	Sex      string    `json:"sex"`
-	OwnerID  uint      `json:"ownerid"`
+	Name     string `json:"name"`
+	Breed    string `json:"breed"`
+	Birthday string `json:"birthday"`
+	Sex      string `json:"sex"`
+	OwnerID  uint   `json:"ownerid"`
 }
 
 func (r *CreatePetRequest) Validate() error {
-	if r.Name == "" && r.Breed == "" && r.Sex == "" && r.OwnerID == 0 {
+	if r.Name == "" && r.Breed == "" && r.Birthday == "" && r.Sex == "" && r.OwnerID == 0 {
 		return fmt.Errorf("request body is empty or malformed")
 	}
 
@@ -30,8 +30,17 @@ func (r *CreatePetRequest) Validate() error {
 	if r.Breed == "" {
 		return errParamIsRequired("Breed", "String")
 	}
+	if r.Birthday == "" {
+		return errParamIsRequired("Birthday", "String")
+	}
+	if _, err := time.Parse("2006-01-02", r.Birthday); err != nil {
+		return fmt.Errorf("param: %s is not a valid date, format accept YYYY-MM-DD", "Birthday")
+	}
 	if r.Sex == "" {
 		return errParamIsRequired("Sex", "String")
+	}
+	if r.OwnerID == 0 {
+		return errParamIsRequired("OwnerID", "Int")
 	}
 	return nil
 }

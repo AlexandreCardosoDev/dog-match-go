0ae21a2ab39823423a0582967a078a0076fb3f81
Cardoso88 <alexmcardoso@gmail.com>
2024-03-20 19:31:09 -0300

    feat: add showPet logic

--- handler/showPet.go
+++ handler/showPet.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/AlexandreCardosoDev/dog-match-go/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func ShowPetHanler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "show pet",
-	})
+func ShowPetHanler(ctx *gin.Context) {
+	id := ctx.Query("id")
+
+	//Validate if id is valid
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	pet := schemas.Pet{}
+	//Find Pet ID
+	if err := db.First(&pet, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("pet with id: %v not found", id))
+		return
+	}
+	//return pet
+	sendSuccess(ctx, "show-pet", pet)
 }
